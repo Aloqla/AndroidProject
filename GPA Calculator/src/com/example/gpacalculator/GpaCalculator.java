96e6564616b4f413b37953f38d124a2e65335f2b
Aloqla <Aaloqla@smu.edu>
2014-12-08 22:35:20 -0600

    GPA Calculator View added

--- GPA Calculator/src/com/example/gpacalculator/GpaCalculator.java
+++ GPA Calculator/src/com/example/gpacalculator/GpaCalculator.java
@@ -4,13 +4,55 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class GpaCalculator extends ActionBarActivity {
 
+	
+	private String[] arraySpinner;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gpa_calculator);
+		
+		
+        this.arraySpinner = new String[] {
+                "A", "B", "C", "D", "F"
+            };
+		
+        Spinner s1 = (Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter adapter = new ArrayAdapter(this,
+            android.R.layout.simple_spinner_item, arraySpinner);
+        s1.setAdapter(adapter);
+        
+        
+        Spinner s2 = (Spinner) findViewById(R.id.spinner2);
+        ArrayAdapter adapter2 = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        s2.setAdapter(adapter2);
+        
+        
+        Spinner s3 = (Spinner) findViewById(R.id.spinner3);
+        ArrayAdapter adapter3 = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        s3.setAdapter(adapter3);
+        
+        
+        Spinner s4 = (Spinner) findViewById(R.id.spinner4);
+        ArrayAdapter adapter4 = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        s4.setAdapter(adapter4);
+        
+        
+        Spinner s5 = (Spinner) findViewById(R.id.spinner5);
+        ArrayAdapter adapter5 = new ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, arraySpinner);
+        s5.setAdapter(adapter5);
+        
+        
+		
 	}
 
 	@Override
